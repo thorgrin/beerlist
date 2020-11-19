5e5a42d50945cabfb9c5fda61f98c0b6fb6cfc70
Petr Velan <petr.velan@gmail.com>
2020-11-19 21:38:36 +0100

    Added statistics for coronavirus and road accidents to beerbot

--- beerbot/beerbot.sh
+++ beerbot/beerbot.sh
@@ -40,6 +40,30 @@ do
 			
 			cat "$cache" | ${CWD}/../tools/json2titles.py > "${CHANNEL_DIR}/in"
 			;;
+		korona)
+#			curl -s https://api.apify.com/v2/key-value-stores/K373S4uCFR9W1K8ei/records/LATEST?disableRedirect=true | jq '"active: " + (.active | tostring) + " | infected: " + (.infected | tostring) + " (+" + (.infectedDaily[-1].value | tostring) + "|+" + (.infected - reduce .infectedDaily[].value as $line (0; . + $line) | tostring) + ") | tested: " + (.totalTested | tostring) + " (+" + (.numberOfTestedGraph[-1].value | tostring) + ") | temporarily feeling better: " + (.recovered | tostring) + " | deceased: " + (.deceased | tostring)' | xargs echo > "${CHANNEL_DIR}/in"
+#			curl -s https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.min.json | jq '"active: " + (.data[-1].kumulativni_pocet_nakazenych - .data[-1].kumulativni_pocet_vylecenych - .data[-1].kumulativni_pocet_umrti | tostring) + " | infected: " + (.data[-1].kumulativni_pocet_nakazenych | tostring) + " (+" + ((.data[-1].kumulativni_pocet_nakazenych - .data[-2].kumulativni_pocet_nakazenych ) | tostring) + ") | tested: " + (.data[-1].kumulativni_pocet_testu | tostring) + " (+" + ((.data[-1].kumulativni_pocet_testu - .data[-2].kumulativni_pocet_testu) | tostring) + ") | temporarily feeling better: " + (.data[-1].kumulativni_pocet_vylecenych | tostring) + " (+" + ((.data[-1].kumulativni_pocet_vylecenych - .data[-2].kumulativni_pocet_vylecenych) | tostring) + ")" + " | deceased: " + (.data[-1].kumulativni_pocet_umrti | tostring) + " (+" + ((.data[-1].kumulativni_pocet_umrti - .data[-2].kumulativni_pocet_umrti) | tostring) + ")"' | xargs echo > "${CHANNEL_DIR}/in"
+			overview=`curl -s https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.min.json`
+			detailed=`curl -s https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.min.json`
+			tests_overall=`echo $overview| jq '.data[0].provedene_testy_celkem'`
+			tests_yesterday=`echo $overview| jq '.data[0].provedene_testy_vcerejsi_den'` # works only after 18:00, data from day before otherwise
+			tests_two_days_ago=`echo $detailed | jq '((.data[-2].kumulativni_pocet_testu - .data[-3].kumulativni_pocet_testu))'`
+			hospitalised=`echo $overview | jq '.data[0].aktualne_hospitalizovani'`
+			active=`echo $overview | jq '.data[0].aktivni_pripady'`
+			infected_overall=`echo $overview | jq '.data[0].potvrzene_pripady_celkem'`
+			infected_today=`echo $overview | jq '.data[0].potvrzene_pripady_dnesni_den'`
+			infected_yesterday=`echo $overview | jq '.data[0].potvrzene_pripady_vcerejsi_den'`
+			infected_two_days_ago=`echo $detailed | jq '((.data[-2].kumulativni_pocet_nakazenych - .data[-3].kumulativni_pocet_nakazenych))'`
+			cured=`echo $overview | jq '.data[0].vyleceni'`
+			cured_yesterday=`echo $detailed | jq '((.data[-1].kumulativni_pocet_vylecenych - .data[-2].kumulativni_pocet_vylecenych))'`
+			deceased=`echo $overview | jq '.data[0].umrti'`
+			deceased_yesterday=`echo $detailed | jq '((.data[-1].kumulativni_pocet_umrti - .data[-2].kumulativni_pocet_umrti))'`
+			positive_tests=`echo "scale=2; 100 * $infected_two_days_ago / $tests_two_days_ago" | bc`
+			echo "active: $active | infected: $infected_overall (+$infected_yesterday, +$infected_today) | tested: $tests_overall (+$tests_yesterday, $positive_tests% positive) | temporarily feeling better: $cured (+$cured_yesterday) | deceased: $deceased (+$deceased_yesterday) | hospitalised: $hospitalised" > "${CHANNEL_DIR}/in"
+			;;
+		nehody)
+			curl -s https://d2g9cow0nr2qp.cloudfront.net/?q=$(echo -n "{ 'from': `date --date='00:00 yesterday' '+%s'`, 'to': `date --date='23:59:59 yesterday' '+%s'`, 'all': 'true' }" | base64) | jq '.["ČR"] | "accidents: " + (.PN | tostring) + " | dead: " + (.M | tostring) + " | serious injury: " + (.TR | tostring) + " | light injury: " + (.LR | tostring)' | xargs echo > "${CHANNEL_DIR}/in"
+			;;
 		*)
 			echo "$nick: tvoje stara je $bar" > "${CHANNEL_DIR}/in"
 			;;
