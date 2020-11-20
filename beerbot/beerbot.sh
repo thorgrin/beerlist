f954a15e1e710bb362f991ec48f0f9227b71f628
Vlastimil Babka <vbabka@suse.cz>
2020-11-20 11:48:45 +0100

    add "first tests" and %positive for yesterday/2 days ago
    There's a new json that has numbers of tests+positive tests without repeated
    tests of same persons. Use it to show how many positive were (the %positive
    is higher than tests including repeated).

--- beerbot/beerbot.sh
+++ beerbot/beerbot.sh
@@ -45,6 +45,7 @@ do
 #			curl -s https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.min.json | jq '"active: " + (.data[-1].kumulativni_pocet_nakazenych - .data[-1].kumulativni_pocet_vylecenych - .data[-1].kumulativni_pocet_umrti | tostring) + " | infected: " + (.data[-1].kumulativni_pocet_nakazenych | tostring) + " (+" + ((.data[-1].kumulativni_pocet_nakazenych - .data[-2].kumulativni_pocet_nakazenych ) | tostring) + ") | tested: " + (.data[-1].kumulativni_pocet_testu | tostring) + " (+" + ((.data[-1].kumulativni_pocet_testu - .data[-2].kumulativni_pocet_testu) | tostring) + ") | temporarily feeling better: " + (.data[-1].kumulativni_pocet_vylecenych | tostring) + " (+" + ((.data[-1].kumulativni_pocet_vylecenych - .data[-2].kumulativni_pocet_vylecenych) | tostring) + ")" + " | deceased: " + (.data[-1].kumulativni_pocet_umrti | tostring) + " (+" + ((.data[-1].kumulativni_pocet_umrti - .data[-2].kumulativni_pocet_umrti) | tostring) + ")"' | xargs echo > "${CHANNEL_DIR}/in"
 			overview=`curl -s https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/zakladni-prehled.min.json`
 			detailed=`curl -s https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/nakazeni-vyleceni-umrti-testy.min.json`
+			detailed_tests=`curl -s https://onemocneni-aktualne.mzcr.cz/api/v2/covid-19/testy.json`
 			tests_yesterday_date=`echo $overview | jq '.data[0].provedene_testy_vcerejsi_den_datum'`
 			infected_yesterday_date=`echo $overview | jq '.data[0].potvrzene_pripady_vcerejsi_den_datum'`
 			tests_overall=`echo $overview| jq '.data[0].provedene_testy_celkem'`
@@ -60,15 +61,18 @@ do
 			cured_yesterday=`echo $detailed | jq '((.data[-1].kumulativni_pocet_vylecenych - .data[-2].kumulativni_pocet_vylecenych))'`
 			deceased=`echo $overview | jq '.data[0].umrti'`
 			deceased_yesterday=`echo $detailed | jq '((.data[-1].kumulativni_pocet_umrti - .data[-2].kumulativni_pocet_umrti))'`
+			tests_first_newest=`echo $detailed_tests | jq '.data[-1].prirustkovy_pocet_prvnich_testu'` # yesterday or 2 days ago, depending on time
 			if [[ $tests_yesterday_date == $infected_yesterday_date ]]; then
 				# this means it's after 18:00 and we can use most recent data
 				positive_tests=`echo "scale=2; 100 * $infected_yesterday / $tests_yesterday" | bc`
+				positive_tests_first=`echo "scale=2; 100 * $infected_yesterday / $tests_first_newest" | bc`
 				tests_prefix="yesterday:"
 			else
 				positive_tests=`echo "scale=2; 100 * $infected_two_days_ago / $tests_two_days_ago" | bc`
+				positive_tests_first=`echo "scale=2; 100 * $infected_two_days_ago / $tests_first_newest" | bc`
 				tests_prefix="2 days ago:"
 			fi
-			echo "active: $active | infected: $infected_overall (+$infected_yesterday, +$infected_today) | tested: $tests_overall ($tests_prefix +$tests_yesterday, $positive_tests% positive) | temporarily feeling better: $cured (+$cured_yesterday) | deceased: $deceased (+$deceased_yesterday) | hospitalised: $hospitalised" > "${CHANNEL_DIR}/in"
+			echo "active: $active | infected: $infected_overall (+$infected_yesterday, +$infected_today) | tested: $tests_overall ($tests_prefix +$tests_yesterday, $positive_tests% positive / +$tests_first_newest, $positive_tests_first%) | temporarily feeling better: $cured (+$cured_yesterday) | deceased: $deceased (+$deceased_yesterday) | hospitalised: $hospitalised" > "${CHANNEL_DIR}/in"
 			;;
 		nehody)
 			curl -s https://d2g9cow0nr2qp.cloudfront.net/?q=$(echo -n "{ 'from': `date --date='00:00 yesterday' '+%s'`, 'to': `date --date='23:59:59 yesterday' '+%s'`, 'all': 'true' }" | base64) | jq '.["ČR"] | "accidents: " + (.PN | tostring) + " | dead: " + (.M | tostring) + " | serious injury: " + (.TR | tostring) + " | light injury: " + (.LR | tostring)' | xargs echo > "${CHANNEL_DIR}/in"
