dfb13260d8969f114b55c3c8633e08c08626e5ce
Petr Velan <petr.velan@gmail.com>
2022-05-31 15:34:36 +0200

    add --compressed parameter to curl calls

--- beerbot/beerbot.sh
+++ beerbot/beerbot.sh
@@ -45,7 +45,7 @@ do
 			today=`date --date="today" +%Y-%m-%d`
 			yesterday=`date --date="yesterday" +%Y-%m-%d`
 			last_week=`date --date="8 days ago" +%Y-%m-%d`
-			overview=`curl -s "https://onemocneni-aktualne.mzcr.cz/api/v3/zakladni-prehled/$today?apiToken=$KORONA_TOKEN" -H 'accept: application/json'`
+			overview=`curl --compressed -s "https://onemocneni-aktualne.mzcr.cz/api/v3/zakladni-prehled/$today?apiToken=$KORONA_TOKEN" -H 'accept: application/json'`
 			active=`echo $overview | jq '.aktivni_pripady'`
 			tests_overall=`echo $overview| jq '.provedene_testy_celkem'`
 			tests_overall_atg=`echo $overview| jq '.provedene_antigenni_testy_celkem'`
@@ -58,7 +58,7 @@ do
 			cured=`echo $overview | jq '.vyleceni'`
 			deceased=`echo $overview | jq '.umrti'`
 
-			reinfections=`curl -s "https://onemocneni-aktualne.mzcr.cz/api/v3/nakazeni-reinfekce?page=1&itemsPerPage=100&datum%5Bbefore%5D=$yesterday&datum%5Bafter%5D=$last_week&apiToken=$KORONA_TOKEN" -H 'accept: application/json'`
+			reinfections=`curl --compressed -s "https://onemocneni-aktualne.mzcr.cz/api/v3/nakazeni-reinfekce?page=1&itemsPerPage=100&datum%5Bbefore%5D=$yesterday&datum%5Bafter%5D=$last_week&apiToken=$KORONA_TOKEN" -H 'accept: application/json'`
 			reinfections_yesterday=`echo "$reinfections" | jq '.[] | select (.datum == "'$yesterday'") | .nove_reinfekce'`
 			reinfections_7d_ago=`echo "$reinfections" | jq '.[] | select (.datum == "'$last_week'") | .nove_reinfekce'`
 			infected_7d_ago=`echo "$reinfections" | jq '.[] | select (.datum == "'$last_week'") | .nove_pripady'`
@@ -66,13 +66,13 @@ do
 			reinfections_week_diff=`echo "$reinfections_yesterday $reinfections_7d_ago" | awk '//{printf("%+d", $1 - $2)}'`
 			positive_tests=`echo "scale=2; 100 * ($infected_yesterday + $reinfections_yesterday) / $tests_yesterday" | bc`
 			
-			detailed_yesterday=`curl -s "https://onemocneni-aktualne.mzcr.cz/api/v3/nakazeni-vyleceni-umrti-testy/$yesterday?apiToken=$KORONA_TOKEN" -H 'accept: application/json'`
+			detailed_yesterday=`curl --compressed -s "https://onemocneni-aktualne.mzcr.cz/api/v3/nakazeni-vyleceni-umrti-testy/$yesterday?apiToken=$KORONA_TOKEN" -H 'accept: application/json'`
 			cured_yesterday=`echo $detailed_yesterday | jq '.prirustkovy_pocet_vylecenych'`
 			deceased_yesterday=`echo $detailed_yesterday | jq '.prirustkovy_pocet_umrti'`
 			
 
 			# There are lots of items, but no more than 10000, which seems to be a limit
-			vaccinations=`curl -s "https://onemocneni-aktualne.mzcr.cz/api/v3/ockovani?page=1&datum%5Bbefore%5D=$yesterday&datum%5Bafter%5D=$yesterday&apiToken=$KORONA_TOKEN&itemsPerPage=10000" -H 'accept: application/json'`
+			vaccinations=`curl --compressed -s "https://onemocneni-aktualne.mzcr.cz/api/v3/ockovani?page=1&datum%5Bbefore%5D=$yesterday&datum%5Bafter%5D=$yesterday&apiToken=$KORONA_TOKEN&itemsPerPage=10000" -H 'accept: application/json'`
 			vaccinations_yesterday=`echo "$vaccinations" | jq '[.[] | select (.datum == "'$yesterday'") | .celkem_davek] | add'`
 			vaccinations_first_yesterday=`echo "$vaccinations" | jq '[.[] | select (.datum == "'$yesterday'") | .prvnich_davek] | add'`
 			vaccinations_second_yesterday=`echo "$vaccinations" | jq '[.[] | select (.datum == "'$yesterday'") | .druhych_davek] | add'`
@@ -86,14 +86,14 @@ do
 			echo "active: $active | infected: $infected_overall (+$infected_yesterday/$reinfections_yesterday [1d], $infected_week_diff/$reinfections_week_diff [1w diff]) | tested (PCR/ATG): $tests_overall/$tests_overall_atg (yesterday +$tests_yesterday/$tests_yesterday_atg, $positive_tests% positive) | temporarily feeling better: $cured (+$cured_yesterday) | deceased: $deceased (+$deceased_yesterday) | hospitalised: $hospitalised | vaccinated: $vaccinations_overall (+$vaccinations_first_yesterday/$vaccinations_second_yesterday/$vaccinations_third_yesterday)" > "${CHANNEL_DIR}/in"
 			;;
 		nehody)
-			curl -s https://d2g9cow0nr2qp.cloudfront.net/?q=$(echo -n "{ 'from': `date --date='00:00 yesterday' '+%s'`, 'to': `date --date='23:59:59 yesterday' '+%s'`, 'all': 'true' }" | base64) | jq '.["ČR"] | "accidents: " + (.PN | tostring) + " | dead: " + (.M | tostring) + " | serious injury: " + (.TR | tostring) + " | light injury: " + (.LR | tostring)' | xargs echo > "${CHANNEL_DIR}/in"
+			curl --compressed -s https://d2g9cow0nr2qp.cloudfront.net/?q=$(echo -n "{ 'from': `date --date='00:00 yesterday' '+%s'`, 'to': `date --date='23:59:59 yesterday' '+%s'`, 'all': 'true' }" | base64) | jq '.["ČR"] | "accidents: " + (.PN | tostring) + " | dead: " + (.M | tostring) + " | serious injury: " + (.TR | tostring) + " | light injury: " + (.LR | tostring)' | xargs echo > "${CHANNEL_DIR}/in"
 			;;
 		btc | eth)
 			#curl -s 'https://www.bitstamp.net/api-internal/market/prices/?step=60&start='`date -u --date="day ago" +%Y-%m-%dT%H:%M:%S.000Z`'&end='`date -u +%Y-%m-%dT%H:%M:%S.999Z`'&pairs=btcusd'  | jq -r '.data[0]["price"] + " " +  .data[0]["history"][0]["open"] + " " + .data[0]["history"][-60]["open"]' | awk '//{printf("BTC: $%.2f | %+.2f%% (1h) | %+.2f%% (24h)\n", $1, 100*$1/$3-100, 100*$1/$2-100)}' > "${CHANNEL_DIR}/in"
-			curl -s https://www.bitstamp.net/api-internal/price-history/${bar}usd/ | jq -r '.data["latest"]["price"] + " " + (.data["prices"]["hour"]["percent_change"]|tostring) + " " + (.data["prices"]["day"]["percent_change"]|tostring) + " " + (.data["prices"]["week"]["percent_change"]|tostring)' | awk '//{printf("%s: $%.2f | %+.2f%% (1h) | %+.2f%% (1d) | %+.2f%% (1w)\n", toupper("'$bar'"), $1, $2, $3, $4)}' > "${CHANNEL_DIR}/in"
+			curl --compressed -s https://www.bitstamp.net/api-internal/price-history/${bar}usd/ | jq -r '.data["latest"]["price"] + " " + (.data["prices"]["hour"]["percent_change"]|tostring) + " " + (.data["prices"]["day"]["percent_change"]|tostring) + " " + (.data["prices"]["week"]["percent_change"]|tostring)' | awk '//{printf("%s: $%.2f | %+.2f%% (1h) | %+.2f%% (1d) | %+.2f%% (1w)\n", toupper("'$bar'"), $1, $2, $3, $4)}' > "${CHANNEL_DIR}/in"
 			;;
 		event | events)
-			curl -s 'https://events.gcm.cz/api.php?action=events&from='`date --date='today' +%Y-%m-%d`'&to='`date --date='next month' +%Y-%m-%d`'&lat=49.1950602&lon=16.6068371&radius=15&requestId=1' | jq -r '.events | reduce .[] as $event (""; . + "(" + $event.gcid + ") " + $event.name + "|" + $event.date_from + "\n" )' | head -n-1 | head -n 4 | while IFS='|' read e d; do echo -n "$e ["`date --date="$d" "+%a %d %b %Y %R"`"] | "; done | sed 's/| $/\n/g' > "${CHANNEL_DIR}/in"
+			curl --compressed -s 'https://events.gcm.cz/api.php?action=events&from='`date --date='today' +%Y-%m-%d`'&to='`date --date='next month' +%Y-%m-%d`'&lat=49.1950602&lon=16.6068371&radius=15&requestId=1' | jq -r '.events | reduce .[] as $event (""; . + "(" + $event.gcid + ") " + $event.name + "|" + $event.date_from + "\n" )' | head -n-1 | head -n 4 | while IFS='|' read e d; do echo -n "$e ["`date --date="$d" "+%a %d %b %Y %R"`"] | "; done | sed 's/| $/\n/g' > "${CHANNEL_DIR}/in"
 			;;
 		*)
 			echo "$nick: tvoje stara je $bar" > "${CHANNEL_DIR}/in"
