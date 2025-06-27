cb329d10285ef302cc9fede0de836224d01806b4
Petr Velan <petr.velan@post.cz>
2025-06-27 19:46:08 +0200

    follow http2 redirects

--- parsers/common.py
+++ parsers/common.py
@@ -31,7 +31,7 @@ def download_html(url: str, user_agent: str = "", headers: Dict = {}) -> str:
 			user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 		headers['Connection'] = 'keep-alive'
 		headers['User-Agent'] = user_agent
-		res = client.get(url, headers=headers)
+		res = client.get(url, headers=headers, follow_redirects=True)
 	finally:
 		client.close()
 	if res.status_code != 200:
